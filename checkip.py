d31417f2c7c68f877780f2709caf350fbc4c2732
Hanabee05 <witankatyes@gmail.com>
2024-11-23 23:24:12 +0700

    Update

---
+++ checkip.py
@@ -0,0 +1,10 @@
+import socket
+
+# Input dari pengguna
+hostname = input('Enter a domain name: ')
+# Mendapatkan alamat IP dari hostname
+ip_address = socket.gethostbyname(hostname)
+
+# Menampilkan hasil
+print(f'Domain Name: {hostname}')
+print(f'IP Address: {ip_address}'
